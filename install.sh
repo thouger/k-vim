b23df6f162a97a5c38cc593ee975c158e84ebde8
lsj <1030490158@qq.com>
2017-05-14 18:58:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -82,18 +82,4 @@ else
 fi
 export SHELL=$system_shell
 
-
-echo "Step4: compile YouCompleteMe"
-echo "It will take a long time, just be patient!"
-echo "If error,you need to compile it yourself"
-echo "cd $CURRENT_DIR/bundle/YouCompleteMe/ && python install.py --clang-completer"
-cd $CURRENT_DIR/bundle/YouCompleteMe/
-git submodule update --init --recursive
-if [ `which clang` ]   # check system clang
-then
-    python install.py --clang-completer --system-libclang   # use system clang
-else
-    python install.py --clang-completer
-fi
-
 echo "Install Done!"
